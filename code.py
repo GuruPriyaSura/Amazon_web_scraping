546179ceb9027392f8e5f265aa3cd8da7173097b
GuruPriyaSura <135823500+GuruPriyaSura@users.noreply.github.com>
2023-06-27 16:36:22 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import datetime
+
+import smtplib
+
+
+
+def check_price():
+    URL = ' ' 
+
+    headers = {"User-Agent": " ", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+    page = requests.get(URL, headers=headers)
+
+    soup1 = BeautifulSoup(page.content, "html.parser")
+
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+    title = soup2.find(id='productTitle').get_text()
+
+    price = soup2.find(id='priceblock_ourprice').get_text()
+
+    price = price.strip()[1:]
+    title = title.strip()
+
+    import datetime
+
+    today = datetime.date.today()
+    
+    import csv 
+
+    header = ['Title', 'Price', 'Date']
+    data = [title, price, today]
+
+    with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+
+# Runs check_price after a set time and inputs data into your CSV
+
+while(True):
+    check_price()
+    time.sleep(86400)
+ 
